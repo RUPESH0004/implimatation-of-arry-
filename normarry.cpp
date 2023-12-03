db8522301ad32686fa44c2c803349bd04aebb473
RUPESH0004 <rj9082356865@gmail.com>
2023-12-03 22:36:48 +0530

    working on the array

---
+++ normarry.cpp
@@ -0,0 +1,14 @@
+//preparing the normal array which is pre-defined arry
+#include<iostream>
+using namespace std;
+int main(){
+    //pre defined array
+    int arry[5]={1,2,3,4,5,};
+    //lets print the arry 
+    cout<<arry[0]<<endl;
+    cout<<arry[1]<<endl;
+    cout<<arry[2]<<endl;
+    cout<<arry[3]<<endl;
+    cout<<arry[4]<<endl;
+   
+}
